85173ae33f06bf6a5a57ccfeacf573f9400a764c
Gabriel Souto <gabriel.souto@ccc.ufcg.edu.br>
2019-06-21 18:32:10 -0300

    Criando comparador pelo codigo da proposta

---
+++ projeto-eco/src/entidades/ComparadorPropostaCodigo.java
@@ -0,0 +1,25 @@
+package entidades;
+
+import java.util.Comparator;
+
+public class ComparadorPropostaCodigo implements Comparator<ProposicaoAbstract> {
+
+    @Override
+    public int compare(ProposicaoAbstract proposicaoAbstract, ProposicaoAbstract t1) {
+        String[] nCadastroAno1 = proposicaoAbstract.getCodigoLei().split("/");
+        String nCadastro1 = nCadastroAno1[0];
+        String ano1 = nCadastroAno1[1];
+        String[] nCadastroAno2 = t1.getCodigoLei().split("/");
+        String nCadastro2 = nCadastroAno2[0];
+        String ano2 = nCadastroAno2[1];
+
+        int result;
+        if(ano1.compareTo(ano2) == 0){
+            result = nCadastro1.compareTo(nCadastro2);
+        }else{
+            result = ano1.compareTo(ano2);
+        }
+
+        return result;
+    }
+}
